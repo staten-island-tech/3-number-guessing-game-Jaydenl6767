032a79374fbbd736d35a9ddb08aad93304d83a84
Jaydenl6767 <Jaydenl321@nycstudents.net>
2025-10-01 08:57:57 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,11 @@
-def guessing()
-import random
-number = random.radint(1,10)
+def guessing():
+import random 
+random_int = random.radint(1,10)
+guessing=int(input("Pick a number from 1-10"))
+if guessing == random_int:
+    print("Correct")
 
+elif guessing > random_int:
+    print("Your number is too high")
+elif guessing < random_int:
+    print("Your number is too low")
