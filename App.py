10db9d358a9d4038bbcdf1f4e2263101fec6a535
Jaydenl6767 <Jaydenl321@nycstudents.net>
2025-10-01 09:33:44 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,11 +1,20 @@
-def guessing():
+win = 0
 import random 
-random_int = random.radint(1,10)
-guessing=int(input("Pick a number from 1-10"))
-if guessing == random_int:
-    print("Correct")
+random_int = random.randint(1,10)
+print(f"Random integer: {random_int}")
 
-elif guessing > random_int:
-    print("Your number is too high")
-elif guessing < random_int:
-    print("Your number is too low")
+while win == 0:
+    guessing=(int(input("Pick a number from 1-10 ")))
+    if guessing == random_int:
+        print("Correct")
+        win=1
+        print(f"you guessed{guess_history}")
+
+    elif guessing > random_int:
+        print("Your number is too high")
+        guess_history.append(guess)
+
+    elif guessing < random_int:
+        print("Your number is too low")
+        guess_history.append(guess)
+
